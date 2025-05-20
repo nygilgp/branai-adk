87ba43cce96d0df83f115e19b4d708b36f2b76e0
Nygil Pereira <nygilgp@gmail.com>
2025-05-20 12:20:34 +0530

    Use custom and build in tools in adk

--- 2-tool-agent/tool_agent/agent.py
+++ 2-tool-agent/tool_agent/agent.py
@@ -1,23 +1,26 @@
+from datetime import datetime
 from google.adk.agents import Agent
 from google.adk.tools import google_search
 
-# def get_current_time() -> dict:
-#     """
-#     Get the current time in the format YYYY-MM-DD HH:MM:SS
-#     """
-#     return {
-#         "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-#     }
+def get_current_time() -> dict:
+    """
+    Get the current time in the format YYYY-MM-DD HH:MM:SS
+    """
+    return {
+        "current_time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    }
 
 root_agent = Agent(
     name="tool_agent",
     model="gemini-2.0-flash",
     description="Tool agent",
+    #     - google_search
+    #     - get_current_time
     instruction="""
     You are a helpful assistant that can use the following tools:
-    - google_search
+    - get_current_time
     """,
-    tools=[google_search],
-    # tools=[get_current_time],
-    # tools=[google_search, get_current_time], # <--- Doesn't work
+    # tools=[google_search],
+    tools=[get_current_time],
+    # tools=[google_search, get_current_time], # <--- Doesn't work, Build in tools doesn't work with custom tools
 )
